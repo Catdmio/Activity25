4dce632a81e4ca4dac45665447189d15ba166b0f
Catdmio <montecinos.catalina@gmail.com>
2018-04-22 23:52:45 -0300

    pushing activity

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sales/new'
-  post 'sales', to: 'sales#create'
   get 'sales/done', to: 'sales#done'
+  post 'sales', to: 'sales#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
